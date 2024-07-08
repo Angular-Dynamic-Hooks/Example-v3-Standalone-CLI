8bb9a863ea78a0d1f7ecab5c2ba090662b729417
Marvin <Mvin@live.de>
2024-07-08 23:15:44 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { HookParserEntry, parseHooks } from 'ngx-dynamic-hooks';
+import { ExampleComponent } from './components/example/example.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// No need for this!
+// bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+// Load components via parseHooks instead
+const parsers: HookParserEntry[] = [
+  ExampleComponent,
+];
+
+parseHooks(document.body, parsers).then(result => {
+  console.log(result);
+});
+
+// Allow other scripts in the DOM to manually parse content again by dispatching events
+document.addEventListener('parseHooks', event => {
+  parseHooks((event as CustomEvent).detail, parsers);
+});
