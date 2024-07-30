0989271a5593611006687886c5abd53f6f6fd480
Marvin <Mvin@live.de>
2024-07-30 21:29:58 +0200

    Finished preliminary version

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
-import { HookParserEntry, parseHooks } from 'ngx-dynamic-hooks';
+import { parseHooks, createProviders, HookParserEntry } from 'ngx-dynamic-hooks';
 import { ExampleComponent } from './components/example/example.component';
+import { CounterService } from './services/counterService';
+import { CounterWriteComponent } from './components/counterWrite/counterWrite.component';
+import { CounterReadComponent } from './components/counterRead/counterRead.component';
+import { WidgetComponent } from './components/widget/widget.component';
+import { NoticeComponent } from './components/notice/notice.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
-// No need for this!
+// No need to load Angular traditionally like this:
 // bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 
-// Load components via parseHooks instead
+// Load components directly into existing HTML via parseHooks() instead
 const parsers: HookParserEntry[] = [
-  ExampleComponent,
+  ExampleComponent,                                       // For example 1
+  { component: WidgetComponent, selector: '.myWidget'},   // For example 2
+  NoticeComponent,                                        // For example 3
+  CarouselComponent                                       // For example 5
 ];
+parseHooks(document.body, parsers);
 
-parseHooks(document.body, parsers).then(result => {
-  console.log(result);
-});
-
-// Allow other scripts in the DOM to manually parse content again by dispatching events
-document.addEventListener('parseHooks', event => {
-  parseHooks((event as CustomEvent).detail, parsers);
-});
+// Use providers scope for example 4
+const scope = createProviders([CounterService]);
+scope.parseHooks(document.body, [CounterWriteComponent, CounterReadComponent]);
